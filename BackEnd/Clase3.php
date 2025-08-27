d74179f68b6dd124f4a315938907d0a71a13ec13
Bernardo Eliezer <berbuzon@gmail.com>
2025-08-26 21:18:36 -0300

    Clase 3 Back

--- BackEnd/Clase3.php
+++ BackEnd/Clase3.php
@@ -51,4 +51,58 @@ echo "<br>";
 for ($x=0; $x <=10 ; $x++) { 
     echo "El número es: $x <br>";
 }
+
+echo "Funciones";
+echo "<br>";
+function mostrar(){
+    echo "Hola Mundo desde una función<br>";
+}
+mostrar();
+
+function suma($a,$b){
+    $resultado=$a+$b;
+    return $resultado;
+}
+echo suma(1,2);
+
+echo "<br>";
+$numero1=40;
+$numero2=20;
+$resultado=suma($numero1,$numero2);
+echo "La suma de $numero1 y $numero2 es: $resultado";
+
+echo "<br>";
+echo $resultado;
+echo "<br>";
+echo $resultado = $numero1 * $numero2;
+echo "<br>";
+function sumara($c,$d){
+    $resultado=$c+$d;
+    return $resultado;
+}
+echo sumara(5,5);
+echo "<br>";
+
+$frase = strtolower("HOLA MUNDO");
+echo $frase;
+echo "<br>";
+$frase = strtoupper("hola mundo");
+echo $frase;
+echo "<br>";
+$frase2 =ucwords("hola mundo");
+echo $frase2;
+echo "<br>";
+$frase3 ="hola mundo";
+echo str_replace("mundo","Carolina",$frase3);
+echo "<br>";
+echo str_replace("MUNDO","Carolina",$frase);
+echo "<br>";
+$frase4="http://localhost/ifts4/index.php";
+$fraseArray=explode("/",$frase4);
+print_r($fraseArray);
+
+echo "<br>";
+echo substr($frase4,-6,5);
+echo "<br>";
+
 ?>
