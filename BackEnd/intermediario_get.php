82cbb9528153d2b4f96c156f00b02ed2a9e79b8e
Bernardo Eliezer <berbuzon@gmail.com>
2025-09-03 07:49:41 -0300

    Clas4_Formulario

---
+++ BackEnd/intermediario_get.php
@@ -0,0 +1,5 @@
+<?php
+$nombre=$_GET['campo_nombre']; // aqui llamo al atributo nombre de la URL
+$apellido=$_GET['campo_apellido']; // aqui llamo al atributo apellido de la URL
+echo $nombre." ".$apellido;
+?>
---
+++ BackEnd/intermediario_post.php
@@ -0,0 +1,5 @@
+<?php
+$nombre=$_POST['campo_nombre']; // nombre del atributo del formulario
+$apellido=$_POST['campo_apellido']; // apellido del atributo del formulario
+echo $nombre." ".$apellido;
+?>
