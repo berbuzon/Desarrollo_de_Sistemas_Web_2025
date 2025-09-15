31534fe32ee1365de14d97d625606bc7f2d5ba34
Bernardo Eliezer <berbuzon@gmail.com>
2025-09-15 09:04:37 -0300

    primer commit

--- BackEnd/TrabajosPracticos/Tarea_N1/analisis_empanadas.php
+++ BackEnd/TrabajosPracticos/Tarea_N1/analisis_empanadas.php
@@ -40,13 +40,56 @@ function mayorVenta($ventas)
     for ($i = 0; $i < count($ventas); $i++) {
         if ($ventas[$i] > $venta_mayor) {
             $venta_mayor = $ventas[$i];
-            $venta_dia = $i+1;
+            $venta_dia = $i + 1;
         }
     }
-    return ['dia'=> $venta_dia, 'cantidad'=> $venta_mayor];
+    return ['dia' => $venta_dia, 'cantidad' => $venta_mayor];
 }
 
-$resultadoMayor = mayorVenta($empanadasVendidas);
-echo "El día de mayor venta fue el " . $resultadoMayor['dia'] ."\n";
+function imprimirVentas($ventas)
+{
+    for ($i = 0; $i < count($ventas); $i++) {
+        echo "Dia " . ($i + 1) . " Cantidad " . $ventas[$i];
+        echo "\n";
+    }
+}
 
-echo "La mayor venta fue de ". $resultadoMayor['cantidad'];
+function promedioVentas($ventas)
+{
+    $sumaEmpanadas = 0;
+    for ($i = 0; $i < count($ventas); $i++) {
+        $sumaEmpanadas = $sumaEmpanadas + $ventas[$i];
+    }
+    return $sumaEmpanadas / count(($ventas));
+}
+
+function menorVenta($ventas)
+{
+    $menorVenta = $ventas[0];
+    $diaMenorVenta = 1;
+    for ($i = 0; $i < count($ventas); $i++) {
+        if ($menorVenta > $ventas[$i]) {
+            $menorVenta = $ventas[$i];
+            $diaMenorVenta = $i+1;
+        }
+    }
+    return ['dia' => $diaMenorVenta, 'cantidad' => $menorVenta];
+}
+
+
+$resultadoMayor = mayorVenta($empanadasVendidas);
+
+echo "El día de mayor venta fue el " . $resultadoMayor['dia'] . "\n";
+
+echo "La mayor venta fue de " . $resultadoMayor['cantidad'];
+echo "\n";
+echo "Listado de ventas diarias: \n";
+imprimirVentas($empanadasVendidas);
+echo "\n";
+echo "El promedio de ventas es : " . round(promedioVentas($empanadasVendidas), 0);
+echo "\n";
+$resultadoMenor = menorVenta($empanadasVendidas);
+echo "El día de menor venta fue el " . $resultadoMenor['dia'];
+echo "\n";
+echo "La menor venta fue de " . $resultadoMenor['cantidad'];
+echo "\n";
