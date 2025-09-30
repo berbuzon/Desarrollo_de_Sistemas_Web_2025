d907ce12c6cdec31ec888427cf27889869ff409a
Bernardo Eliezer <berbuzon@gmail.com>
2025-09-30 20:17:24 -0300

    grid

--- BackEnd/Crud/borrar.php
+++ BackEnd/Crud/borrar.php
@@ -4,7 +4,7 @@ include 'conn.php';
 $sql = "DELETE FROM datos_usuarios WHERE id_usuarios = $idb";
 if ($conn->query($sql) === true) {
     header("Location: ./select.php");
-    echo "<a href='./form.php'>Regresar</a>";
+    echo "<a href='./form.php'>Regresar</a>"; ## este echo no se muestra por el header
     exit();
 } else {
     echo "Error deleting record: " . $conn->error;
